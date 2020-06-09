767a182914f070b5ba69a349dd0e969c77ee5ae1
gang3 <gang3@LAPTOP-SFBKCUO3>
2020-06-09 11:15:54 +0900

    Hello messages

--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -3,8 +3,9 @@ public class MyHello
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello Java!");
-		System.out.println("Hello Java 2nd!");
+		System.out.println("Hello1!");
+		System.out.println("Hello2!");
+		System.out.println("Hello3!");
 	}
 
 
