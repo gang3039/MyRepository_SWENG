a0079c928579ef6d94ced3c84df2e9e5c26b15c4
gang3039 <gang3039@naver.com>
2020-06-11 12:04:35 +0900

    This is 2nd A,B,C

--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -4,7 +4,7 @@ public class B
 	public static void main(String args[])
 	{
 		System.out.println("B");
-
+		System.out.println("This is 2nd B");
 	}
 
 }
--- MyGit/src/C.java
+++ MyGit/src/C.java
@@ -4,7 +4,7 @@ public class C
 	public static void main(String args[])
 	{
 		System.out.println("C");
-
+		System.out.println("This is 2nd C");
 	}
 
 }
